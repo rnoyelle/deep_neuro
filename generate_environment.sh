5899fd7edbcadaa73bcbe459d556642d8fcba2fe
rnoyelle <39985092+rnoyelle@users.noreply.github.com>
2018-11-07 13:41:10 +0100

    Update generate_environment.sh

--- generate_environment.sh
+++ generate_environment.sh
@@ -8,8 +8,9 @@ conda create -n deep_neuro python=3.5 numpy scipy scikit-learn pandas matplotlib
 # Generate tree
 cd ..
 mkdir -p data/raw
-mkdir -p results/training/pvals
-mkdir -p results/training/summary
-mkdir -p results/training/plots
+mkdir -p results/training
+mkdir -p results/pvals
+mkdir -p results/summary
+mkdir -p results/plots
 mkdir -p scripts/_params
 mv deep_neuro/ scripts/
