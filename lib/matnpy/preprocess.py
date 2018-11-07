f466226a28e5acc51c308fe21a990dd5ae482eec
rnoyelle <39985092+rnoyelle@users.noreply.github.com>
2018-11-07 14:01:07 +0100

    Update preprocess.py

--- lib/matnpy/preprocess.py
+++ lib/matnpy/preprocess.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 19 14:33:12 2017
+@author: jannes
+"""
+
 import os.path
- 
- 
 import numpy as np
 from scipy.signal import butter, lfilter
 from .matnpyio import get_sfreq
