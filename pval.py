b35d8ec0f81dbca8a2d446d1214287c763d7d41c
rnoyelle <39985092+rnoyelle@users.noreply.github.com>
2018-11-07 15:26:45 +0100

    Add files via upload

--- pval.py
+++ pval.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import os.path
+import re
 
 import pandas as pd
 import numpy as np
@@ -10,8 +11,10 @@ from scipy.special import erf
 
 def pval(n_test, r, classes):
     """
-    return the p_value for the null hypothesis
+    returns the p_value for the null hypothesis
     """
+    n_test = np.array(eval(n_test.replace('.',',')))
+    
     # Pi = np.array( classes * [1/classes] )
     # Pi = 0.5 + n_test * (1 - classes/2)/np.sum(n_test)
     # std = np.sqrt( np.sum( Pi * (1 -Pi)/n_test ) )/classes
@@ -19,7 +22,7 @@ def pval(n_test, r, classes):
     
     return 1 - erf( abs( r - 1/classes)/ (np.sqrt(2) * std ) ) 
 
-def sign(p)
+def sign(p):
     '''
     return the sign according of the p_value
     '''
@@ -27,33 +30,59 @@ def sign(p)
     return(sign)
 
 ### PATH
-base_path =  # .../my_project/
-#raw_path = base_path + 'data/raw/' # .../my_project/data/raw/
+base_path = '/home/rudy/Python/rnoyelle_github_test/'  # .../my_project/
+#raw_path = '/media/rudy/disk2/lucy/' # .../my_project/data/raw/
 
-### SESSION and DECODERS       
-session = 
-#session = os.listdir(raw_path)
-#session.remove('unique_recordings.mat')
 
+
+# DECODER
 decoders = ['stim', 'resp'] # ['stim']
-
 
 for decode_for in decoders :
     if decode_for == 'stim':
         classes = 5
     else:
         classes = 2
-    
-    
+
+    ### SESSION
+    # this way or
+    #session = os.listdir(raw_path)
+    #session.remove('unique_recordings.mat')
+    # or this : 
+    available_file = os.listdir(base_path + 'results/training/')
+    session = []
+    for i in range(len(available_file)):
+        txt = re.split('_|\.', available_file[i]) # = [sess_no, 'training', decode_for', 'csv']
+        if txt[2] == decode_for :
+            session.append(txt[0])
+
     ## merge files
+    print(len(session))
     df_session = len(session) * [0]
-    
+
     for count, sess_no in enumerate(session):
         file_name = base_path + 'results/training/'+ sess_no + '_training_'+decode_for+'.csv'
         df_session[count] = pd.read_csv(file_name)
-        
-    result = pd.concat(df_session, ignore_index=True)
+
+
+    if len(session) > 1 :
+        result = pd.concat(df_session, ignore_index=True)
+    elif len(session) == 1 :
+        result = df_session[0]
+    else:
+        print('No file for decode_for = ', decode_for)
+        break
+
     
+    result = result[ ['session', 'decode_for', 'only_correct_trials',
+                      'areas', 'cortex', 'elec_type',
+                      'frequency_band',
+                      'interval',
+                      'recall_macro', 'error_bar_th', 'error_bar_emp',
+                      'seed', 'n_splits',
+                      'n_test_per_class', 'data_size', 'n_chans', 'window_size'] ]
+    
+  
     
     # select columns
     #result = result
@@ -83,17 +112,18 @@ for decode_for in decoders :
     result['sign'] = result.apply(lambda row : sign(row.pval), axis=1)
     
     # Save file
-    file_name = base_path + 'results/pvals/'
-            + 'pval_all_sess_'+decode_for+'.csv'
+    file_name = (base_path + 'results/pvals/'
+            + 'pval_all_sess_'+decode_for+'.csv')
     file_exists = os.path.isfile(file_name)
     # if file already exist, just append the data.
     if file_exists :
-        with open(file_name, 'a') as f:
-            df.to_csv(f, mode ='a', index=False, header=False)
+        print(file_name," already exists. Summary file hasn't been saved.")
+        # to append to the file
+        #with open(file_name, 'a') as f:
+            #df.to_csv(f, mode ='a', index=False, header=False)
     else:
         with open(file_name, 'w') as f:
-            df.to_csv(f, mode ='w', index=False, header=True)
-            
-            
+            result.to_csv(f, mode ='w', index=False, header=True)
+        print(file_name, " Succesfully saved")
 
         
--- summary.py
+++ summary.py
@@ -4,21 +4,33 @@ import os.path
 
 import pandas as pd
 import numpy as np
+import re
 
 
+# PATH
 
-base_path =  # .../my_project/
-#raw_path = base_path + 'data/raw/' # .../my_project/data/raw/
+base_path = '/home/rudy/Python/rnoyelle_github_test/'  # .../my_project/
+#raw_path = '/media/rudy/disk2/lucy/' # .../my_project/data/raw/
 
           
-session = 
-#session = os.listdir(raw_path)
-#session.remove('unique_recordings.mat')
 
+## DECODERS
 decoders = ['stim', 'resp'] # ['stim']
 
 for decode_for in decoders :
     
+    ## SESSION 
+    # this way :
+    #session = os.listdir(raw_path)
+    #session.remove('unique_recordings.mat')
+    # or this way
+    available_file = os.listdir(base_path + 'results/training/')
+    session = []
+    for i in range(len(available_file)):
+        txt = re.split('_|\.', available_file[i]) # = [sess_no, 'training', decode_for', 'csv']
+        if txt[2] == decode_for :
+            session.append(txt[0])
+            
     
     ## merge files
     df_session = len(session) * [0]
@@ -26,7 +38,13 @@ for decode_for in decoders :
     for count, sess_no in enumerate(session):
         df_session[count] = pd.read_csv(base_path + 'results/training/' + sess_no + '_training_'+decode_for+'.csv')
         
-    result = pd.concat(df_session, ignore_index=True)
+    if len(session) > 1 :
+        result = pd.concat(df_session, ignore_index=True)
+    elif len(session) == 1 :
+        result = df_session[0]
+    else:
+        print('No file for decode_for = ', decode_for)
+        break
     
     
     # select columns
@@ -51,17 +69,19 @@ for decode_for in decoders :
                            'recall_macro_train_per_fold', 'recall_macro_test_per_fold',
                            'seed', 'n_splits',
                            'data_size', 'n_chans', 'window_size',
-                           'y_true_per_fold', 'y_predict_per_fold'] ]
+                           'y_true_per_fold', 'y_pred_per_fold'] ]
     
     # Save to file
-    file_name = base_path + 'results/summary/'
-            + 'summary_all_sess_'+decode_for+'.csv'
+    file_name = (base_path + 'results/summary/'
+            + 'summary_all_sess_'+decode_for+'.csv')
     file_exists = os.path.isfile(file_name)
     if file_exists :
-        with open(file_name, 'a') as f:
-            df.to_csv(f, mode ='a', index=False, header=False)
+        print(file_name," already exists. Summary file hasn't been saved.")
+        #with open(file_name, 'a') as f:
+            #result.to_csv(f, mode ='a', index=False, header=False)
     else:
         with open(file_name, 'w') as f:
-            df.to_csv(f, mode ='w', index=False, header=True)
+            result.to_csv(f, mode ='w', index=False, header=True)
+        print(file_name, " Succesfully saved")
         
         
