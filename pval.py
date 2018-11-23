af93b9fed2e86d3ed833d19e9a139800c13539ac
rnoyelle <39985092+rnoyelle@users.noreply.github.com>
2018-11-23 11:44:48 +0100

    Add files via upload

--- pval.py
+++ pval.py
@@ -29,6 +29,27 @@ def sign(p):
     sign = '***' if p < .001 else '**' if p < .01 else '*' if p < .05 else 0
     return(sign)
 
+def get_interval_name(interval):    
+    
+    if interval == 'align_on'+'sample'+'from_time'+str(-500)+'to_time'+str(0)  :
+        return('pre-sample')
+    
+    elif interval == 'align_on'+'sample'+'from_time'+str(0)+'to_time'+str(500) :
+        return('sample')
+    
+    elif interval == 'align_on'+'sample'+'from_time'+str(500)+'to_time'+str(1000) :
+        return('delay-start')
+    
+    elif interval == 'align_on'+'match'+'from_time'+str(-500)+'to_time'+str(0) :
+        return('delay-end')
+        #return('pre-match')
+    
+    elif interval == 'align_on'+'match'+'from_time'+str(0)+'to_time'+str(500) :
+        return('match')
+    
+    else:
+        return(interval)
+
 ### PATH
 base_path = # .../my_project/
 #raw_path = # .../my_project/data/raw/
@@ -72,55 +93,30 @@ for decode_for in decoders :
     else:
         print('No file for decode_for = ', decode_for)
         break
+    
+    result = result[ ['session', 'decode_for', 'only_correct_trials',
+                      'areas', 'cortex', 'elec_type',
+                      'frequency_band',
+                      'interval',
+                      'mean_per_class_accuracy', 'error_bar', 'n_test_per_class',
+                      'seed', 'n_splits',
+                      'data_size', 'n_chans', 'window_size']
+    
 
-    
-    result = result[ ['session', 'decode_for', 'only_correct_trials',
-                      'areas', 'cortex', 'elec_type',
-                      'frequency_band',
-                      'interval',
-                      'recall_macro', 'error_bar_th', 'error_bar_emp',
-                      'seed', 'n_splits',
-                      'n_test_per_class', 'data_size', 'n_chans', 'window_size'] ]
-    
-  
-    
-    # select columns
-    #result = result
-                               #'renorm',
-                           #'n_layers','patch_dim', 'pool_dim',
-                           #'channels_in', 'channels_out',
-                           #'nonlin','fc_units',
-                           #'n_iterations', 'size_of_batches', 'learning_rate',
-                           #'weights_dist', 'normalized_weights',
-                           #'batch_norm',
-                           #'keep_prob_train',
-                           #'l2_regularization_penalty',
-                           #'time']
-     
-    # keep only these columns
-    result = result[ ['session', 'decode_for', 'only_correct_trials',
-                      'areas', 'cortex', 'elec_type',
-                      'frequency_band',
-                      'interval',
-                      'recall_macro', 'error_bar_th', 'error_bar_emp',
-                      'seed', 'n_splits',
-                      'n_test_per_class', 'data_size', 'n_chans', 'window_size'] ]
-    
-#     ['session', 'decode_for', 'only_correct_trials', 'interval',
-#        'interval_name', 'areas', 'cortex', 'elec_type', 'frequency_band',
-#        'mean_per_class_accuracy', 'error_bar', 'pval', 'sign', 'data_size',
-#        'n_chans', 'window_size', 'n_splits', 'seed']
     
     ## ADD pval and sign
     
-    result['pval'] = result.apply(lambda row: pval(row.n_test_per_class, row.recall_macro, classes), axis=1)
+    result['pval'] = result.apply(lambda row: pval(row.n_test_per_class, row.mean_per_class_accuracy, classes), axis=1)
     result['sign'] = result.apply(lambda row : sign(row.pval), axis=1)
+    
+    ## add get_interval_name
+    result['interval_name'] = result.apply(lambda row : get_interval_name(row.interval), axis=1)
     
     # Save file
     file_name = (base_path + 'results/pvals/'
             + 'pval_all_sess_'+decode_for+'.csv')
     file_exists = os.path.isfile(file_name)
-    # if file already exist, just append the data.
+    # if file already exist, print a warning message ( decomment to append the data ) 
     if file_exists :
         print(file_name," already exists. Pval file hasn't been saved.")
         # to append to the file
--- summary.py
+++ summary.py
@@ -46,29 +46,19 @@ for decode_for in decoders :
         break
     
     
-    # 
-    #result = result
-                               #'renorm',
-                           #'n_layers','patch_dim', 'pool_dim',
-                           #'channels_in', 'channels_out',
-                           #'nonlin','fc_units',
-                           #'n_iterations', 'size_of_batches', 'learning_rate',
-                           #'weights_dist', 'normalized_weights',
-                           #'batch_norm',
-                           #'keep_prob_train',
-                           #'l2_regularization_penalty',
-                           #'time']
      
-    # columns to keep
+    # columns to keep    
     result = result[ ['session', 'decode_for', 'only_correct_trials',
-                           'areas', 'cortex', 'elec_type',
-                           'frequency_band',
-                           'interval',
-                           'recall_macro', 'error_bar_th', 'error_bar_emp', 'n_test_per_class',
-                           'recall_macro_train_per_fold', 'recall_macro_test_per_fold',
-                           'seed', 'n_splits',
-                           'data_size', 'n_chans', 'window_size',
-                           'y_true_per_fold', 'y_pred_per_fold'] ]
+                      'areas', 'cortex', 'elec_type',
+                      'frequency_band',
+                      'interval',
+                      'mean_per_class_accuracy', 'error_bar', 'error_bar_emp', 'n_test_per_class',
+                      'mean_per_class_accuracy_train_per_fold', 'mean_per_class_accuracy_test_per_fold',
+                      'seed', 'n_splits',
+                      'data_size', 'n_chans', 'window_size',
+                      'y_true_per_fold', 'y_pred_per_fold'] ]
+    
+    
     
     # Save to file
     file_name = (base_path + 'results/summary/'
